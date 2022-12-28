2d0278eacc39f1c1a51ccc39f953194d5525af4e
Me <118435243+MEyoussef@users.noreply.github.com>
2022-12-28 18:48:08 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,2 @@
 // api link https://www.themealdb.com/api/json/v1/1/random.php
 
-// make a connection
-let ApiConnection = new XMLHttpRequest();
-ApiConnection.open("GET", "https://www.themealdb.com/api/json/v1/1/random.php");
-ApiConnection.send();
-let json = JSON.parse(this.responseText);
-console.log(json)
