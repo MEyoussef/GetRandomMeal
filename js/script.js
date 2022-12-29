e4d2989b88f7895ad08dfa622f077fb3262154ec
Me <118435243+MEyoussef@users.noreply.github.com>
2022-12-29 22:15:57 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-// https://valorant-api.com/v1/agents
-fetch("https://www.themealdb.com/api/json/v1/1/random.php").then((data) => data.json()).then((data) => {
-    // console.log(data)
-    let btn = document.querySelector(".btn");
-    btn.addEventListener('click', () => {
-        fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-            .then(res => res.json())
-            .then(res => {
-                document.querySelector("h3").innerText = `${res.meals[0].strMeal}`;
-                document.querySelector(".meal-name").innerText = `${res.meals[0].idMeal}`;
-                document.querySelector("img").src = `${res.meals[0].strMealThumb}`
-            })
+// https://www.themealdb.com/api/json/v1/1/random.php
+let Api = "https://www.themealdb.com/api/json/v1/1/random.php";
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+    fetch(Api).then((data) => data.json()).then((data) => {
+        document.querySelector("img").src = `${data.meals[0].strMealThumb}`;
+        document.querySelector("h3").innerText = `${data.meals[0].strMeal}`;
+        document.querySelector(".meal-id").innerText = `${data.meals[0].idMeal}`;
+        document.querySelector(".category").innerText = `category : ${data.meals[0].strCategory}`;
+        document.querySelector(".area").innerText = `area : ${data.meals[0].strArea}`;
+        document.querySelector(".tags").innerText = `tags : ${data.meals[0].strTags}`;
+        if (data.meals[0].strTags === null) {
+            document.querySelector(".tags").innerText = `tags : no tags`;
+        }
     });
-});
+})
