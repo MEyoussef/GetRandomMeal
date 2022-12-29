f99120c0e36982ce7bb714bd2fa18c632f06c7d3
Me <118435243+MEyoussef@users.noreply.github.com>
2022-12-29 16:54:11 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-// api link https://www.themealdb.com/api/json/v1/1/random.php
-
+// https://valorant-api.com/v1/agents
+fetch("https://www.themealdb.com/api/json/v1/1/random.php").then((data) => data.json()).then((data) => {
+    // console.log(data)
+    let btn = document.querySelector(".btn");
+    btn.addEventListener('click', () => {
+        fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+            .then(res => res.json())
+            .then(res => {
+                document.querySelector("h3").innerText = `${res.meals[0].strMeal}`;
+                document.querySelector(".meal-name").innerText = `${res.meals[0].idMeal}`;
+                document.querySelector("img").src = `${res.meals[0].strMealThumb}`
+            })
+    });
+});
